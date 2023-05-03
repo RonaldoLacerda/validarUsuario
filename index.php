02b5b0fbc0212044e0af6911591e1e39ce7e43bd
RonaldoLacerda <116221034+RonaldoLacerda@users.noreply.github.com>
2023-05-03 11:12:27 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+function validarUsuario(array $usuario)
+{
+    if(empty($usuario['codigo']) || empty($usuario['nome']) || empty($usuario['idade'])) {
+        throw new Exception "Campos obrigatórios não foram preenchidos!";
+    
+    }
+
+    return true;
+}
+
+$usuario = [
+    'codigo' => 1,
+    'nome'=> '',
+    'idade' => 57
+];
+
+try {
+    $status = validarUsuario($usuario);
+} catch (Exception $e) {
+    echo $e->getMessage();
+    return false
+}
+
+
+echo "teste";
+
+?>
